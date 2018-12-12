521ccf94946c33bb07c6302193313fc991a065b5
Hugo Meder <hugomeder@gmail.com>
2018-12-12 02:05:55 +0100

    and also MainView.java

--- AndroidStudioProjects/Fractals/app/src/main/java/de/bitsnarts/android/apps/fractals/MainView.java
+++ AndroidStudioProjects/Fractals/app/src/main/java/de/bitsnarts/android/apps/fractals/MainView.java
@@ -7,6 +7,7 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import de.bitsnarts.shared.math.transforms.ConformalAffineTransform2D ;
 
 import de.bitsnarts.android.apps.fractals.graphics.Mandelbrot;
 import de.bitsnarts.android.apps.fractals.graphics.SimpleFractalPainter;
@@ -17,14 +18,9 @@ public class MainView extends View {
     private Paint canvasPaint;
     private Mandelbrot iter;
     private int[] colors;
-    private int cx, cy ;
     private boolean redraw = true ;
-    private float downX, downY ;
-    private double dx, dy ;
     private Paint textPaint;
-    private int pointerCount;
-    private boolean drawLine;
-    private float px1, py1, px2, py2 ;
+    private Dragger dragger;
 
     public MainView(Context context, AttributeSet attrs){
         super(context, attrs);
@@ -54,81 +50,36 @@ public class MainView extends View {
         super.onSizeChanged(w, h, oldw, oldh);
         canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
         drawCanvas = new Canvas(canvasBitmap) ;
-        //SimpleFractalPainter.paint ( drawCanvas, x0, y0, dx, dy, colors, iter ) ;
-        //drawCanvas.drawRGB ( 0, 0, 0 ) ;
+        w = canvasBitmap.getWidth();
+        h = canvasBitmap.getHeight() ;
+        double dw = 4.0 ;
+        double dh = 4.0 ;
+        double scale = dw/(w-1) ;
+        // maps pixel coords to graphic coords
+        ConformalAffineTransform2D tr = ConformalAffineTransform2D.createFGromAngleAndScale(0, scale, -w * scale / 2, -h * scale / 2);
+        dragger = new Dragger ( tr ) ;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         if ( redraw ) {
-            int w = canvasBitmap.getWidth();
-            int h = canvasBitmap.getHeight() ;
-            double dw = 4.0 ;
-            double dh = 4.0 ;
-            dx = dw/(w-1) ;
-            dy = dx ;
-            double x0 = -2.0-cx ;
-            double y0 = -2.0-cy ;
-            SimpleFractalPainter.paint ( canvasBitmap, x0, y0, dx, dy, colors, iter ) ;
+            SimpleFractalPainter.paint ( canvasBitmap, dragger.getTransform (), colors, iter ) ;
             redraw = false ;
         }
         canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
-        drawText ( canvas ) ;
-        if ( drawLine )
-            drawLine ( canvas ) ;
-    }
-
-    private void drawLine(Canvas canvas) {
-        canvas.drawLine( px1, py1, px2, py2, textPaint );
-        float dx = (py1-py2)/2 ;
-        float dy =-(px1-px2)/2 ;
-        canvas.drawLine( px1+dx, py1+dy, px1-dx, py1-dy, textPaint );
-
-    }
-
-    private void drawText(Canvas canvas) {
-        float ts = textPaint.getTextSize();
-        float y = ts ;
-        canvas.drawText( "pointerCount "+pointerCount, ts, y, textPaint );
-        y += ts ;
+        dragger.drawState ( canvas ) ;
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        float touchX = event.getX();
-        float touchY = event.getY();
-        int pc = event.getPointerCount() ;
-        if ( pc != pointerCount ) {
-            pointerCount = pc ;
+
+        boolean rv = dragger.onTouchEvent(event);
+        if ( rv ) {
+            if ( event.getAction() == MotionEvent.ACTION_UP )
+                redraw = true ;
+            invalidate();
         }
-        drawLine = false ;
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                downX = touchX ;
-                downY = touchY ;
-                break;
-            case MotionEvent.ACTION_MOVE:
-                if ( pointerCount == 2 ) {
-                    px1 = event.getX( 0 ) ;
-                    py1 = event.getY( 0 ) ;
-                    px2 = event.getX( 1 ) ;
-                    py2 = event.getY( 1 ) ;
-                    drawLine = true ;
-                }
-                //drawPath.lineTo(touchX, touchY);
-                break;
-            case MotionEvent.ACTION_UP:
-                cx += (touchX-downX)*dx ;
-                cy += (touchY-downY)*dy ;
-                initRedraw () ;
-                //drawCanvas.drawPath(drawPath, drawPaint);
-                //drawPath.reset();
-                break;
-            default:
-                return false;
-        }
-        invalidate();
-        return true;
+        return rv ;
     }
 
     private void initRedraw() {
