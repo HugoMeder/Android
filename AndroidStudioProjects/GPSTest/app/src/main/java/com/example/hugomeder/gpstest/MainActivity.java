a7cfb99b7b2193d617909cac2b97a763719e03e0
Hugo Meder <hugomeder@gmail.com>
2018-12-18 01:04:35 +0100

    A frist version of GPSTest

---
+++ AndroidStudioProjects/GPSTest/app/src/main/java/com/example/hugomeder/gpstest/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.hugomeder.gpstest;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if ( location == null )
+                    textView.append( "?");
+                else
+                    textView.append( "\n"+location.getLatitude()+" "+location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent (Settings.ACTION_LOCATION_SOURCE_SETTINGS) ;
+                startActivity(intent);
+            }
+        };
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) {
+            boolean no_perm1 = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED;
+            boolean no_perm2 = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED;
+            if ( no_perm1 && no_perm2 ) {
+                requestPermissions(new String[]{
+                                Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.INTERNET}
+                        , 10);
+                return;
+            } else {
+                configureButton();
+            }
+        }else{
+            configureButton () ;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case 10:
+                if ((grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED))
+                    configureButton();
+                else
+                    System.out.println ( "No permission?" ) ;
+                return;
+        }
+    }
+
+    private void configureButton() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    List<String> providers = locationManager.getAllProviders();
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
+                    Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);;
+                    textView.append( "\ndata...");
+                    locationListener.onLocationChanged( loc );
+                } catch ( Throwable th ) {
+                    th.printStackTrace();
+                }
+            }
+        });
+    }
+    }
+
